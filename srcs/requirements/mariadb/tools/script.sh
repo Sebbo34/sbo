3532c0bc216ab17334079b7b11aeae7f1daefbca
Sebastien BO <seb@MacBook-Air-de-Sebastien.local>
2024-10-14 14:46:47 +0200

    maj

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 # Démarrer MySQL avec mysqld_safe
+rm -rf /var/run/mysqld && mkdir /var/run/mysqld && chown mysql /var/run/mysqld/
+pkill -9 mysqld
 mysqld_safe &
-
+/etc/init.d/mysql start
 # Attendre que MySQL démarre
 until mysqladmin ping &>/dev/null; do
   echo "Waiting for MySQL to start..."
