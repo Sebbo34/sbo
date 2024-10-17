1f4d32923579a7d41f85f3d4aa97fcdc06ac0b0a
Sebastien BO <seb@MacBook-Air-de-Sebastien.local>
2024-10-17 15:14:08 +0200

    maj

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,4 +1,3 @@
-export SQL_DATABASE=my_database SQL_USER=my_user SQL_PASSWORD=my_password SQL_ROOT_PASSWORD=my_root_password WP_PATH=/var/www/html/wordpress
 
 # Démarrer MySQL avec mysqld_safe en arrière-plan
 mysqld_safe &
