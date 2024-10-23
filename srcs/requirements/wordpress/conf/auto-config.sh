e4da7dcde908891cbb0e2617b944773cd9886f9c
Sebastien BO <seb@MacBook-Air-de-Sebastien.local>
2024-10-24 00:07:55 +0200

    change

--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -9,7 +9,7 @@ if [ ! -f wp-config.php ]; then
 						--dbname=$SQL_DATABASE \
 						--dbuser=$SQL_USER \
 						--dbpass=$SQL_PASSWORD \
-						--dbhost=mariadb:3306 --path='/var/www/wordpress'
+						--dbhost=mariadb:3306 --path='/var/www/wordpress/'
     wp core install --allow-root --url=localhost --title=Site_Title --admin_user=admin_username --admin_password=admin_password --admin_email=your@email.com
     wp user create login email [--role=user] [--user_pass=password] [--allow-root]
 fi
