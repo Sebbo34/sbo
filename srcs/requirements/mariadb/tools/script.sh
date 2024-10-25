1dbe02ca14830e520a7fbecdb6139cd262647222
Sebastien BO <seb@MacBook-Air-de-Sebastien.local>
2024-10-25 16:08:25 +0200

    change

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+#set -eux
 
-service mysql start
-sleep 10
-echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+service mysql start;
 
-mysql < db1.sql
+# log into MariaDB as root and create database and the user
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p'12345' shutdown
-
+mysqladmin -u root -p${SQL_ROOT_PASSWORD} shutdown
+#mysqladmin -u root shutdown
 exec mysqld_safe
+
+#print status
+echo "MariaDB database and user were created successfully! "
