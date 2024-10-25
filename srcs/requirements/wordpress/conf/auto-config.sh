40b61aa72c3e595ba2178d08a78221879914c61b
Sebastien BO <seb@MacBook-Air-de-Sebastien.local>
2024-10-25 14:06:21 +0200

    change

--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -44,4 +44,4 @@ if [ ! -d /run/php ]; then
 fi
 
 # start the PHP FastCGI Process Manager (FPM) for PHP version 7.3 in the foreground
-exec /usr/sbin/php-fpm7.3 -F -R
+exec /usr/sbin/php-fpm7.4 -F -R
