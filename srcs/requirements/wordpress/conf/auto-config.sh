702d01db7c0bb573359a44977a3a811478ce9744
Sebastien BO <seb@MacBook-Air-de-Sebastien.local>
2024-10-26 18:19:45 +0200

    change

--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
 cd /var/www/html/wordpress
+rm wp-config.php
+if [ ! -f wp-config.php ]; then
+    wp config create	--allow-root \
+						--dbname="$SQL_DATABASE" \
+						--dbuser="$SQL_USER" \
+						--dbpass="$SQL_PASSWORD" \
+						--dbhost=mariadb:3306 --path='/var/www/wordpress/'
+    wp core install --allow-root --url=https://localhost --title=Site_Title --admin_user=admin_username --admin_password=admin_password --admin_email=your@email.com
+    wp user create login email --role=user --user_pass=password --allow-root
+fi
 mkdir /run/php
 exec /usr/sbin/php-fpm7.4 -F -R
