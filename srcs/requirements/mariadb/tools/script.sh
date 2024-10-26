21b73809067e13c6ab18b6de240443e29529a27b
Sebastien BO <seb@MacBook-Air-de-Sebastien.local>
2024-10-26 18:02:58 +0200

    change

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 mysqld_safe --skip-networking &
-sleep 10
 echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' ;" >> db1.sql
@@ -12,4 +11,4 @@ mysql < db1.sql
 
 mysqladmin -u root -p'12345' shutdown
 
-mysqld_safe
+exec mysqld_safe
