f4995fe2b3c2008908186ce33e0e3027c6d2755b
Sebastien BO <seb@MacBook-Air-de-Sebastien.local>
2024-10-27 18:33:59 +0100

    change

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'my_database' );
+define( 'DB_NAME', 'inception' );
 
 /** MySQL database username */
 define( 'DB_USER', 'dbuser' );
