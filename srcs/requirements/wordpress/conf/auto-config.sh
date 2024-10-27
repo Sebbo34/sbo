53b5460e20f0385cdeb6b7e35efb8e466d4d65e6
Sebastien BO <seb@MacBook-Air-de-Sebastien.local>
2024-10-27 18:45:21 +0100

    change

--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-
+mkdir /var/www/html/wordpress
 cd /var/www/html/wordpress
+wp core dowload
 mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wp-config.php
 
 mv /wp-config.php /var/www/html/wordpress/wp-config.php
