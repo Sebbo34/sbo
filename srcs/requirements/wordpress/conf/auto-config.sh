3fa1f385f8afe57eaf8d93e5d3ce01e0dc762b7a
Sebastien BO <seb@MacBook-Air-de-Sebastien.local>
2024-10-27 19:51:58 +0100

    change

--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -3,9 +3,9 @@ cd /var/www/html/wordpress
 wp core dowload --allow-root
 curl -O https://raw.githubusercontent.com/WordPress/WordPress/master/wp-config-sample.php && \
     cp wp-config-sample.php wp-config.php
-   	sed -i "s/database_name_here/inception/" wp-config.php && \
-    sed -i "s/username_here/dbuser/" wp-config.php && \
-    sed -i "s/password_here/dbpassword/" wp-config.php && \
+   	sed -i "s/database_name_here/my_database/" wp-config.php && \
+    sed -i "s/username_here/my_user/" wp-config.php && \
+    sed -i "s/password_here/my_password/" wp-config.php && \
     sed -i "s/localhost/mariadb:3306/" wp-config.php
 
 wp core install --allow-root --url=https://localhost --title=Site_Title --admin_user=admin_username --admin_password=admin_password --admin_email=your@email.com
