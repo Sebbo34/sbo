a7382d284f0fc59394ea605be0543ad5225e8d5f
Sebastien BO <seb@MacBook-Air-de-Sebastien.local>
2024-10-27 18:40:21 +0100

    change

--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 cd /var/www/html/wordpress
-mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
+mv /var/www/html/wordpress/wp-config-sample.php /var/www/html/wp-config.php
 
-mv /wp-config.php /var/www/html/wp-config.php
+mv /wp-config.php /var/www/html/wordpress/wp-config.php
 
 wp core install --allow-root --url=https://localhost --title=Site_Title --admin_user=admin_username --admin_password=admin_password --admin_email=your@email.com
 wp user create login email --role=user --user_pass=password --allow-root
