a0d8aaba3f0139924f6668d0fe19443ab80fc95a
Sebastien Bo <sebbo0304@gmail.com>
2024-10-28 17:28:10 +0100

    change

--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -3,16 +3,16 @@ cd /var/www/html/wordpress
 wp core download --allow-root
 curl -O https://raw.githubusercontent.com/WordPress/WordPress/master/wp-config-sample.php && \
     cp wp-config-sample.php wp-config.php
-   	sed -i "s/database_name_here/$SQL_DATABASE/" wp-config.php && \
-    sed -i "s/username_here/$SQL_USER/" wp-config.php && \
-    sed -i "s/password_here/$SQL_PASSWORD/" wp-config.php && \
-    sed -i "s/localhost/$SQL_HOST/" wp-config.php
+   	sed -i "s/database_name_here/my_database/" wp-config.php && \
+    sed -i "s/username_here/my_user/" wp-config.php && \
+    sed -i "s/password_here/my_password/" wp-config.php && \
+    sed -i "s/localhost/mariadb:3306/" wp-config.php
 sleep 5
-wp core install --allow-root --url=https://localhost --title=$SITE_TITLE --admin_user=$ADMIN --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL
+wp core install --allow-root --url=https://$DOMAINE_NAME --title=$SITE_TITLE --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL
 
 wp user create	--allow-root \
-			${USER_LOGIN} ${USER_MAIL} \
+			${USER1_LOGIN} ${USER1_MAIL} \
 			--role=author \
-			--user_pass=${USER_PASS} ;
+			--user_pass=${USER1_PASS} ;
 mkdir /run/php
 exec /usr/sbin/php-fpm7.4 -F -R
