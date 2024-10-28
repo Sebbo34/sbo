b238d492dae877da70ab71fa14c27584f3009dd1
Sebastien Bo <sebbo0304@gmail.com>
2024-10-28 18:44:44 +0100

    change

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -5,7 +5,7 @@ sleep 10
 echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345';" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '12345' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
 mysql < db1.sql
