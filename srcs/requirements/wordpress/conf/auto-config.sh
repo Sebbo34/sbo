bea0a2786ce5f09243b20d24819322c10a5a487e
Sebastien BO <seb@MacBook-Air-de-Sebastien.local>
2024-10-28 13:07:10 +0100

    change

--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -7,8 +7,8 @@ curl -O https://raw.githubusercontent.com/WordPress/WordPress/master/wp-config-s
     sed -i "s/username_here/my_user/" wp-config.php && \
     sed -i "s/password_here/my_password/" wp-config.php && \
     sed -i "s/localhost/mariadb:3306/" wp-config.php
-
-wp core install --allow-root --url=https://localhost --title=Site_Title --admin_user=admin_username --admin_password=admin_password --admin_email=your@email.com
+sleep 15
+wp core install --allow-root --url=https://sbo.42.fr --title=Site_Title --admin_user=admin_username --admin_password=admin_password --admin_email=your@email.com
 wp user create login email --role=user --user_pass=password --allow-root
 mkdir /run/php
 exec /usr/sbin/php-fpm7.4 -F -R
