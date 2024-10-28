05f54b23c1f50373f5fd93aef887fa5e611b7353
Sebastien Bo <sebbo0304@gmail.com>
2024-10-28 16:49:21 +0100

    change

--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -3,10 +3,10 @@ cd /var/www/html/wordpress
 wp core download --allow-root
 curl -O https://raw.githubusercontent.com/WordPress/WordPress/master/wp-config-sample.php && \
     cp wp-config-sample.php wp-config.php
-   	sed -i "s/database_name_here/my_database/" wp-config.php && \
-    sed -i "s/username_here/my_user/" wp-config.php && \
-    sed -i "s/password_here/my_password/" wp-config.php && \
-    sed -i "s/localhost/mariadb:3306/" wp-config.php
+   	sed -i "s/database_name_here/$SQL_DATABASE/" wp-config.php && \
+    sed -i "s/username_here/$SQL_USER/" wp-config.php && \
+    sed -i "s/password_here/$SQL_PASSWORD/" wp-config.php && \
+    sed -i "s/localhost/$SQL_HOST/" wp-config.php
 sleep 5
 wp core install --allow-root --url=https://$DOMAINE_NAME --title=$SITE_TITLE --admin_user=$ADMIN --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL
 
