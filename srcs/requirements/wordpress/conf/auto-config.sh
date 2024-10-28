dab9029358f467f287f833fa29465eca5dd01bba
Sebastien Bo <sebbo0304@gmail.com>
2024-10-28 18:25:26 +0100

    change

--- srcs/requirements/wordpress/conf/auto-config.sh
+++ srcs/requirements/wordpress/conf/auto-config.sh
@@ -7,11 +7,11 @@ curl -O https://raw.githubusercontent.com/WordPress/WordPress/master/wp-config-s
     sed -i "s/username_here/$SQL_USER/" wp-config.php && \
     sed -i "s/password_here/$SQL_PASSWORD/" wp-config.php && \
     sed -i "s/localhost/$SQL_HOST/" wp-config.php
-sleep 15
+sleep 5
 wp core install --allow-root --url=https://localhost --title=$SITE_TITLE --admin_user=$ADMIN --admin_password=$ADMIN_PASSWORD --admin_email=$ADMIN_EMAIL
 wp user create	--allow-root \
 			${USER_LOGIN} ${USER_MAIL} \
 			--role=author \
-			--user_pass=${USER_PASS} ;
+			--user_pass=${USER_PASS};
 mkdir /run/php
 exec /usr/sbin/php-fpm7.4 -F -R
