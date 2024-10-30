39d1dab6ab815224acb6b80159193824cb32092c
Sebastien Bo <sebbo0304@gmail.com>
2024-10-30 15:14:33 +0100

    change

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 mysqld_safe --skip-networking
-sleep 5
 echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' ;" >> db1.sql
