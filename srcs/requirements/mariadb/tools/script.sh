4788be5073ed315db7ca447a1663b197e9a46d7e
Sebastien Bo <sebbo0304@gmail.com>
2024-10-30 16:35:04 +0100

    change

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "SAAAAAAAAAAAAAAAAAAAALU"
+/etc/init.d/mariadb start
 echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE ;" > db1.sql
 echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD' ;" >> db1.sql
 echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' ;" >> db1.sql
